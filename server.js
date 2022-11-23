60334b9e341fd7c84e0f4a02a3543cbac8eea742
Ugochukwu Chukwuma <ceeugochukwu@gmail.com>
2022-11-23 11:16:46 +0100

    cleanup

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const SHA256 = require('crypto-js/sha256');
 const e = require('express');
 const Block = require('./block.js');
 const Blockchain= require("./simpleChain.js");
-const blockchain = new Blockchain();
+const ugoChain = new Blockchain();
 
 /**
  * Controller Definition to encapsulate routes to work with blocks
@@ -29,7 +29,7 @@ class BlockController {
     getBlockByIndex() {
         this.app.get("/api/block/:index", async (req, res) => {
             // Add your code here
-            res.send(await blockchain.getBlock(req.params.index));
+            res.send(await ugoChain.getBlock(req.params.index));
         });
     }
 
@@ -40,10 +40,10 @@ class BlockController {
         this.app.post("/api/block", async (req, res) => {
             
             if (req.body.data){
-                let newBlockHeight = await blockchain.addBlock(new Block(req.body.data));
+                let newBlockHeight = await ugoChain.addBlock(new Block(req.body.data));
                 // console.log("New height is " + newBlockHeight);
                 
-                res.send(await blockchain.getBlock(newBlockHeight));
+                res.send(await ugoChain.getBlock(newBlockHeight));
 
             }
             else {
@@ -55,25 +55,25 @@ class BlockController {
     // Validate a Block in the Blockchain 
     validateBlock() {
         this.app.get("/api/validate/block/:index", async (req, res) => {
-            res.send(await blockchain.validateBlock(req.params.index));
+            res.send(await ugoChain.validateBlock(req.params.index));
         })
     }
 
     // Validate the Blockchain
     validateChain() {
         this.app.get("/api/validate/chain", async (req, res) => {
-            res.send(await blockchain.validateChain());
+            res.send(await ugoChain.validateChain());
         })
     }
     /**
      * Help method to inizialized Mock dataset, adds 10 test blocks to the blocks array
      */
     async initializeMockData() {
-        let height = await blockchain.getBlockHeight();
+        let height = await ugoChain.getBlockHeight();
         if (height === 0) {
             (function theLoop(i) {
                 setTimeout(function () {
-                    blockchain.addBlock(new Block("Block " + i)).then(() =>{
+                    ugoChain.addBlock(new Block("Block " + i)).then(() =>{
                         if (--i) theLoop(i);
                     })
                 }, 100);
@@ -87,7 +87,7 @@ class BlockController {
 setTimeout(function () {
     console.log("-------");
     console.log("Validating chain");
-    blockchain.validateChain().then;
+    ugoChain.validateChain().then;
 }, 2500)
 
 /**
